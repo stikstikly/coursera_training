95d93d385a4fb470221199710a943a109b53410c
stikstikly <74784828+stikstikly@users.noreply.github.com>
2020-11-20 15:03:25 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -5,13 +5,14 @@ df = pd.read_csv(file, names=['MY COLUMN A','MY COLUMN C'])
 df = pd.read_csv(file, usecols=['MY COLUMN A','MY COLUMN C'])
 
 ## Reorder columns by column name
-df = df[['A', 'D', 'C', 'B']]
+df = df[['A', 'D', 'C', 'Z']]
 
-## Get unique values from a column
-df['MY COLUMN'].unique()
+
 
 ## Insert column at position
 df.insert(3, 'MY COLUMN Z', 'Default Value')
 
 ## Return distinct rows based on column value via dropping duplicates
 df = df.drop_duplicates(subset ='MY COLUMN', keep = 'first')
+
+## New
